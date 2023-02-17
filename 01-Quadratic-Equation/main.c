ad7ab7f2de83ead1ab4f76268e1fccbfa9b9da76
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-02-16 22:38:41 -0600

    Update main.c

--- 01-Quadratic-Equation/main.c
+++ 01-Quadratic-Equation/main.c
@@ -26,7 +26,7 @@ int main(int argc, char** args)
    
    if(disc > 0.0)
     {
-       printf("\t\t\tLos dos valores son reales");
+       printf("\t\t\t Both discriminants are positive values");
        x1 = ((-b + sqrt(disc)) / (2.0 * a));
        x2 = ((-b - sqrt(disc)) / (2.0 * a));
        printf("\n\t\t\t x1=%.2f   x2=%.2f", x1, x2);

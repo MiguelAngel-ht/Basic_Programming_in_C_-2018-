f181afb81754f47ca227564dcd24655aa06bea1d
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-02-22 20:04:34 -0600

    Add files via upload

---
+++ 02-Conditionals/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+ {
+   int numero,mayor=0,menor=999999,i;
+for (i=0;i<3;i=i+1)
+{
+printf("Ingrese el %i numero: ",i+1);
+scanf("%i",& numero);
+
+     if(numero > mayor)
+        mayor = numero;
+
+         if(numero<menor)
+            menor=numero;
+}
+
+printf("El mayor es: %i\n",mayor);
+printf("El menor es: %i\n",menor);
+
+
+return 0;
+}

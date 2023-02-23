182892107becf9ef4f49ff311845ccbad9f3e302
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-02-22 20:11:17 -0600

    Update main.c

--- 02-Conditionals/main.c
+++ 02-Conditionals/main.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
-int main()
- {
-   int numero,mayor=0,menor=999999,i;
-for (i=0;i<3;i=i+1)
-{
-printf("Ingrese el %i numero: ",i+1);
-scanf("%i",& numero);
 
-     if(numero > mayor)
-        mayor = numero;
+int main(){
+ 
+  int numero, mayor = 0, menor = 999999, i;
+  
+ for(i = 0; i < 3; i = i+1){
+ 
+  printf("Ingrese el %i numero: ",i+1);
+  scanf("%i",& numero);
 
-         if(numero<menor)
-            menor=numero;
-}
+  if(numero > mayor)
+       mayor = numero;
 
-printf("El mayor es: %i\n",mayor);
-printf("El menor es: %i\n",menor);
+   if(numero < menor)
+       menor=numero;
+  }
+
+  printf("El mayor es: %i\n",mayor);
+  printf("El menor es: %i\n",menor);
 
 
-return 0;
+  return 0;
 }

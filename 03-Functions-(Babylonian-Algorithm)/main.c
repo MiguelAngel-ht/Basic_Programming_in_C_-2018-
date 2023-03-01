09e30b077264ff14c9da800c05d8e482660749c3
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-02-28 21:02:09 -0600

    Add files via upload

---
+++ 03-Functions-(Babylonian-Algorithm)/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+float calcular_raiz_cuadrada(float x)
+
+{
+
+
+if (x<0)
+
+ {
+
+   x=x*(-1);
+
+ }
+
+ float b = x;
+
+
+
+ while (b*b != x)
+
+  {
+
+
+     b = .5*((x/b)+ b);
+
+   }
+
+
+   return b;
+
+}
+
+
+int main()
+
+{
+
+ float x,b;
+
+ printf("Raiz cuadrada de un número \n");
+
+ printf("Raíz x: ");
+    scanf("%f",&x);
+
+
+
+
+  b = calcular_raiz_cuadrada(x);
+
+
+if (x<0)
+
+ {
+
+    printf("Raíz imaginaria: %.4f \n",b);
+
+  }
+
+else
+
+  {
+
+      printf("Raíz real: %.4f \n",b);
+
+  }
+
+
+  return 0;
+
+}

e0ff9cdcf65bb3e5b6b10aa5a368508a6b20868a
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-03-30 21:19:18 -0600

    Update main.c

--- 05-Pointers/main.c
+++ 05-Pointers/main.c
@@ -1,26 +1,31 @@
+// Libraries
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definition of functions
 void imprimirArreglo(int[], int);
 void ordenburbuja(int[], int);
 
 int main()
-{
+{   
+    // Definition of variables
     int n = 100;
     int *arr[n];
     int i;
     
     for (i = 0; i < n; i++)
     {
-        arr[i]=rand()%500;
+        arr[i] = rand() % 500;
     }
     
     printf("El arreglo original es %d\n\n",n);
     imprimirArreglo(arr, n);
 
-    printf("El  arreglo ordenado por ALGORITMO DE BURBUJA es \n");
-    ordenburbuja (arr, n);
+    printf("El arreglo ordenado por ALGORITMO DE BURBUJA es \n");
+    
+    ordenburbuja(arr, n);
     printf("\n");
+    
     return 0;
 }
 
